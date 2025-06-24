6293721f24729b6f8fff0985e2ea0536196d5544
shams <shamsenroute@gmail.com>
2025-06-24 11:03:14 +0600

    design issue

--- js/main.js
+++ js/main.js
@@ -4,63 +4,19 @@
     /** ------------------------------------
     * Window loader
     * -------------------------------------*/
+  $('#toggleButton').click(function () {
+    const content = $('#toggleContent');
 
-    $(window).on("load", function () {
-      $('.loading-wrap').fadeOut();
-    });
-    
-    /** ------------------------------------
-    * Stellarnav
-    * -------------------------------------*/
+    if (content.hasClass('activebtn')) {
+        content.slideUp(function () {
+            content.removeClass('activebtn');
+        });
+    } else {
+        content.slideDown(function () {
+            content.addClass('activebtn');
+        });
+    }
+  });
 
-    /** ------------------------------------
-    * Magnificpopup video
-    * -------------------------------------*/
-
-
-    /** ------------------------------------
-    * Lightbox
-    * -------------------------------------*/
-  
-
-
-    /** ------------------------------------
-    * StickyHeader
-    * -------------------------------------*/
-
-    // function stickyHeader(){
-    //   var headerFixed = $(".header-area");
-    //   if ($(window).scrollTop() > 60) {
-    //       headerFixed.addClass("animated fadeInDown stricky-menu");
-    //   }
-    //   else {
-    //       headerFixed.removeClass("animated fadeInDown stricky-menu");
-    //   }
-    // }
-
-    
-    /** ------------------------------------
-    * Pagination list
-    * -------------------------------------*/
-
-    // $(".pagination-list li a").on('click' ,function(e){
-    //     e.preventDefault();
-    //     // remove classname 'active' from all li who already has classname 'active'
-    //     $(".pagination-list li a.active").removeClass("active"); 
-    //     // adding classname 'active' to current click li 
-    //     $(this).addClass("active"); 
-    // });
-
-
-
-    
-  /** ------------------------------------
-    * Window on scroll function
-  * -------------------------------------*/
- 
-    // $(window).on("scroll", function () {
-    //   stickyHeader();
-    // });
-   
 })(jQuery);
 
